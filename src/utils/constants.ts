4b7a88316d9e56c90b751d8dddf8bd956e0592dc
Matheus Antunes <mta.santos@gmail.com>
2025-06-14 22:47:17 -0400

    fix: favorites key on constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://restcountries.com/v3.1';
-export const FAVORITES_KEY = 'FAVORITES_KEY';
+export const BASE_URL = "https://restcountries.com/v3.1";
+export const FAVORITES_KEY = "favorites";
